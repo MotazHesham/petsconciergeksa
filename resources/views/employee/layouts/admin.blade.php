acd2d866ea5ecf7840a955db7c48417e0dbe02a5
Motaz Hesham <motazhesham01@gmail.com>
2023-07-24 05:44:00 +0300

    fixings for assets

--- resources/views/employee/layouts/admin.blade.php
+++ resources/views/employee/layouts/admin.blade.php
@@ -9,20 +9,20 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ trans('panel.site_title') }}</title>
-    <link rel="stylesheet" href="{{ asset('dashboard_offline/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('dashboard_offline/css/font-awesome.css') }}">
-    <link rel="stylesheet" href="{{ asset('dashboard_offline/css/all.css') }}">
-    <link rel="stylesheet" href="{{ asset('dashboard_offline/css/jquery.dataTables.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('dashboard_offline/css/dataTables.bootstrap4.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('dashboard_offline/css/buttons.dataTables.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('dashboard_offline/css/select.dataTables.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('dashboard_offline/css/select2.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('dashboard_offline/css/bootstrap-datetimepicker.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('dashboard_offline/css/coreui.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('dashboard_offline/css/font-awesome.css') }}">
-    <link rel="stylesheet" href="{{ asset('dashboard_offline/css/dropzone.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('dashboard_offline/css/perfect-scrollbar.min.css') }}">
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet" /> 
+    <link rel="stylesheet" href="{{ asset('public/dashboard_offline/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/dashboard_offline/css/font-awesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/dashboard_offline/css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/dashboard_offline/css/jquery.dataTables.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/dashboard_offline/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/dashboard_offline/css/buttons.dataTables.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/dashboard_offline/css/select.dataTables.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/dashboard_offline/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/dashboard_offline/css/bootstrap-datetimepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/dashboard_offline/css/coreui.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/dashboard_offline/css/font-awesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/dashboard_offline/css/dropzone.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/dashboard_offline/css/perfect-scrollbar.min.css') }}">
+    <link href="{{ asset('public/css/custom.css') }}" rel="stylesheet" /> 
     @if (app()->getLocale() == 'ar')
         <style>
             .c-sidebar-nav .c-sidebar-nav-dropdown-items {
@@ -109,30 +109,30 @@
     </div>
     @include('sweetalert::alert')
 
-    <script src="{{ asset('dashboard_offline/js/jquery.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/popper.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/perfect-scrollbar.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/coreui.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/dataTables.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/dataTables.buttons.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/buttons.flash.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/buttons.html5.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/buttons.print.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/buttons.colVis.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/pdfmake.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/vfs_fonts.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/jszip.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/dataTables.select.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/popper.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/coreui.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/buttons.flash.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/buttons.colVis.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/jszip.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/dataTables.select.min.js') }}"></script>
     <script src="https://cdn.ckeditor.com/ckeditor5/16.0.0/classic/ckeditor.js"></script>
-    <script src="{{ asset('dashboard_offline/js/moment.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/bootstrap-datetimepicker.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/select2.full.min.js') }}"></script>
-    <script src="{{ asset('dashboard_offline/js/dropzone.min.js') }}"></script>
-    <script src="{{ asset('plugins/bootstrap-tagsinput/bootstrap-tagsinput.js') }}"></script>
-    <script src="{{ asset('js/simplebar.js') }}"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/moment.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/bootstrap-datetimepicker.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/select2.full.min.js') }}"></script>
+    <script src="{{ asset('public/dashboard_offline/js/dropzone.min.js') }}"></script>
+    <script src="{{ asset('public/plugins/bootstrap-tagsinput/bootstrap-tagsinput.js') }}"></script>
+    <script src="{{ asset('public/js/simplebar.js') }}"></script>
+    <script src="{{ asset('public/js/main.js') }}"></script>
 
     <!-- SweetAlert2 -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.min.css">
